89966aa3414be053ea310639097995baa66757f8
Tomasz Smolarczyk <smolarczyk.thomas@gmail.com>
2023-07-26 21:41:00 +0200

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+import * as basicLightbox from "basiclightbox";
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`);
+
+instance.show();
